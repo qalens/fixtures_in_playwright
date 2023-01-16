412a4a9eb288f920ac8306776143c9b92f7ff778
Atmaram Naik <naik.atmaram@gmail.com>
2023-01-16 18:59:19 +0530

    ATM | made tokens constatn at global level

--- fixtures/request.fixture.ts
+++ fixtures/request.fixture.ts
@@ -1,6 +1,6 @@
 import {APIRequestContext, expect, test as base} from "@playwright/test"
+const tokens:Record<string,string>={}
 class AuthenticatedRequest {
-    static tokens:Record<string,string>={}
     constructor(private request:APIRequestContext){}
     private login = async (username:string,password:string)=>{
         const resp = await this.request.post("http://localhost:8080/v3/user/login",{
@@ -15,8 +15,8 @@ class AuthenticatedRequest {
     }
     post = async (username:string,password:string,url:string,data:any)=>{
         const tokenKey = `${username}|${password}`
-        if(!AuthenticatedRequest.tokens[tokenKey]){
-            AuthenticatedRequest.tokens[tokenKey]=await this.login(username,password)
+        if(!tokens[tokenKey]){
+            tokens[tokenKey]=await this.login(username,password)
         }
         return this.request.post(url,{
             data,
